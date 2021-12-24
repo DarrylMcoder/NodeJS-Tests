44cf59fd61160231dccdd877f5d089b60ef7693b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-24 09:16:32 -0500

    Add files via upload

--- caesarShift.js
+++ caesarShift.js
@@ -1,6 +1,6 @@
 var caesarShift = function (str, amount) {
   console.log('Str: ' + str);
-  console.log('StrType: ' + (typeof str));
+  console.log('StrType: ' + (typeof str === 'string'));
   // Wrap the amount
   if (amount < 0) {
     return caesarShift(str, amount + 26);
