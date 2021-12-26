bfcafe1e472420ec361f431b1c671060df805d93
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-26 10:48:29 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,58 +4,97 @@ var http = require('http'),
     gzip = zlib.createGzip(),
     inflate = zlib.createInflate(),
     deflate = zlib.createDeflate(),
-    serveStatic = require('serve-static'),
-    finalhandler = require('finalhandler'),
     connect = require('connect'),
     app = connect(),
     httpProxy = require('http-proxy'),
+    proxy = httpProxy.createServer(),
     Caesar = require('./caesar.js'),
-    caesar = Caesar.createStream(),
+    //caesar = Caesar.createStream(),
     port = process.env.PORT || 80;
 
+proxy.on('proxyRes', processRes);
+
+proxy.on('error', (e,req,res) => {
+  res.writeHead(500,'Internal server error',{});
+  res.end('Something went wrong. \n<br> Error: ' + e);
+});//*/
 
 app.use((req, res, next) => {
-  req.on('error', (err) => console.log('Request error: ' + err));
-  res.on('error', (err) => console.log('Response error: ' + err));
-  next();
-});
-
-
-app.use(function (req, res) {
   if(!req.url.match(/^\/http/g)) {
     res.statusCode = 400;
     res.end("Incomplete request URL \n");
     console.log("Incomplete request:" + req.url);
-    return;
+  }else{
+    next();
   }
+});
+
+app.use((req, res) => {
   proxy.web(req, res, {
-    target: path2Proxy(req.url),
-    ignorePath: true,
-    changeOrigin: true,
-    selfHandleResponse: true,
-    secure: false,
-    followRedirects: true
+      target: path2Proxy(req.url),
+      ignorePath: true,
+      changeOrigin: true,
+      selfHandleResponse: true,
+      //autoRewrite: true,
+      followRedirects: true
   });
 });
-
 http.createServer(app).listen(port);
+  
+  function path2Proxy(url) {
+    return url.replace(/^\//g, '');
+  }
 
-var proxy = httpProxy.createProxyServer({});
+function processRes(proxyRes, req, res) {
+  if (proxyRes && proxyRes.headers) {
+    contentEncoding = proxyRes.headers['content-encoding'];
+    // Delete the content-length if it exists. Otherwise, an exception will occur
+    if ('content-length' in proxyRes.headers) {
+      delete proxyRes.headers['content-length'];
+    }
+  }
+  
+  let unzip, zip;
+  // Now only deal with the gzip/deflate/undefined content-encoding.
+  switch (contentEncoding) {
+    case 'gzip':
+      unzip = zlib.Gunzip();
+      zip = zlib.Gzip();
+      break;
+    case 'deflate':
+      unzip = zlib.Inflate();
+      zip = zlib.Deflate();
+      break;
+    case 'br':
+      unzip = zlib.BrotliDecompress && zlib.BrotliDecompress();
+      zip = zlib.BrotliCompress && zlib.BrotliCompress();
+      break;
+  }
+  
+  let _write = res.write;
+  let _end = res.end;
+  let _res = res;
+  let caesar = Caesar.createStream();
+  
+  
+    if (unzip) {
+    unzip.on('error', function (e) {
+      console.log('Unzip error: ', e);
+      _end.call(res);
+    });
+    handleCompressed(res, _res, caesar, unzip, zip, callback);
+  } else if (!contentEncoding) {
+    handleUncompressed(res, _write, _end, callback);
+  } else {
+    console.log('Not supported content-encoding: ' + contentEncoding);
+  }
+}
 
-proxy.on('error', (err, req, res) => console.log("Proxy error: " + err));
+function handleCompressed(res, _res, caesar, unzip, zip, callback) {
 
-proxy.on('proxyReq', (proxyReq, req, res) => {
-  proxyReq.setHeader('Accept-Encoding', '');
-})
+  res.write = data => { unzip.write(data) };
 
-proxy.on('proxyRes', (proxyRes, req, res) => {
-  console.log(proxyRes.headers['content-encoding']);
-  proxyRes.pipe(Caesar.createStream()).pipe(res);
-  return;
-});
-
-process.on('warning', e => console.warn(e.stack));
-
-function path2Proxy(url) {
-  return url.replace(/^\//g, '');
+  res.end = () => unzip.end();
+  
+  unzip.pipe(caesar).pipe(zip).pipe(_res);
 }
