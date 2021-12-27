549fa7b1ca1646bd7e035833b055ac3a40d69ca6
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-27 09:30:57 -0500

    Add files via upload

---
+++ encodeContent.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Transform = require("stream").Transform;
+
+const caesarShift = require('./caesarShift.js').caesarShift;
+
+module.exports = function(config) {
+  function encodeContent(data) {
+    if (config.processContentTypes.includes(data.contentType)) {
+      data.stream = data.stream.pipe(
+        new Transform({
+          decodeStrings: false,
+          transform: function (chunk, encoding, next) {
+            var updated = caesarShift(chunk.toString(), 1);
+            this.push(updated);
+            next();
+          },
+        })
+      );
+    }
+  }
+  
+  
+};
--- server.js
+++ server.js
@@ -1,111 +1,27 @@
-var http = require('http'),
-    zlib = require('zlib'),
-    gunzip = zlib.createGunzip(),
-    gzip = zlib.createGzip(),
-    inflate = zlib.createInflate(),
-    deflate = zlib.createDeflate(),
-    connect = require('connect'),
-    app = connect(),
-    httpProxy = require('http-proxy'),
-    proxy = httpProxy.createServer(),
-    Caesar = require('./caesar.js'),
-    //caesar = Caesar.createStream(),
-    port = process.env.PORT || 80;
+var express = require("express"),
+    unblocker = require("unblocker"),
+    encodeContent = require('./encodeContent.js'),
+    port = process.env.PORT || 80,
+    app = express();
 
-proxy.on('proxyRes', processRes);
+app.use(
+  unblocker({
+    responseMiddleware: [
+      encodeContent({
+        processContentTypes: [
+          "text/html",
+          "text/javascript",
+          "text/css",
+        ],
+      }),
+    ],
+  })
+);
 
-proxy.on('error', (e,req,res) => {
-  res.writeHead(500,'Internal server error',{});
-  res.end('Something went wrong. \n<br> Error: ' + e);
-});//*/
+app.get("/", (req, res) =>
+  res.redirect("/proxy/https://en.wikipedia.org/wiki/Main_Page")
+);
 
-app.use((req, res, next) => {
-  if(!req.url.match(/^\/http/g)) {
-    res.statusCode = 400;
-    res.end("Incomplete request URL \n");
-    console.log("Incomplete request:" + req.url);
-  }else{
-    next();
-  }
-});
+app.listen(port);
 
-app.use((req, res) => {
-  proxy.web(req, res, {
-      target: path2Proxy(req.url),
-      ignorePath: true,
-      changeOrigin: true,
-      //selfHandleResponse: true,
-      autoRewrite: true,
-      followRedirects: true
-  });
-});
-http.createServer(app).listen(port);
-  
-  function path2Proxy(url) {
-    return url.replace(/^\//g, '');
-  }
-
-function processRes(proxyRes, req, res) {
-  if (proxyRes && proxyRes.headers) {
-    contentEncoding = proxyRes.headers['content-encoding'];
-    // Delete the content-length if it exists. Otherwise, an exception will occur
-    if ('content-length' in proxyRes.headers) {
-      delete proxyRes.headers['content-length'];
-    }
-  }
-  
-  let unzip, zip;
-  // Now only deal with the gzip/deflate/undefined content-encoding.
-  switch (contentEncoding) {
-    case 'gzip':
-      unzip = zlib.Gunzip();
-      zip = zlib.Gzip();
-      break;
-    case 'deflate':
-      unzip = zlib.Inflate();
-      zip = zlib.Deflate();
-      break;
-    case 'br':
-      unzip = zlib.BrotliDecompress && zlib.BrotliDecompress();
-      zip = zlib.BrotliCompress && zlib.BrotliCompress();
-      break;
-  }
-  
-  let _write = res.write;
-  let _end = res.end;
-  let _res = res;
-  let caesar = Caesar.createStream();
-  
-  
-    if (unzip) {
-    unzip.on('error', function (e) {
-      console.log('Unzip error: ', e);
-      _end.call(res);
-    });
-    handleCompressed(res, _res, caesar, unzip, zip);
-  } else if (!contentEncoding) {
-    handleUncompressed(res, _res, caesar);
-  } else {
-    console.log('Not supported content-encoding: ' + contentEncoding);
-  }
-}
-
-function handleCompressed(res, _res, caesar, unzip, zip) {
-
-  res.write = data => { unzip.write(data) };
-
-  res.end = () => unzip.end();
-  
-  unzip.pipe(caesar).pipe(zip).pipe(_res);
-}
-
-
-
-function handleUncompressed(res, _res, caesar) {
-
-  res.write = data => { caesar.write(data) };
-
-  res.end = () => caesar.end();
-  
-  caesar.pipe(_res);
-}
+console.log("app listening on port 8080. Test at http://localhost:8080/");
