f5fc725042c52b6d3e546b8306bfc599838f8f16
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-28 08:08:14 -0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+self.importScripts('caesarShift.js');
+
+self.addEventListener('install', function(event){
+      // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+    self.skipWaiting();
+	console.log(event);
+});
+
+self.addEventListener('activate', function(event){
+    console.log(event);
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async function() {
+    return fetch(event.request)
+    .then(response => {
+      return response.text()
+      .then(text => caesarShift(text, -1))
+      .then(text => {
+        return new Response("Response: " + text, 
+        {
+          headers: response.headers
+        });
+      }).catch(e => {
+        return new Response("Error: " + e);
+      });
+    })
+    .catch(e => {
+      return new Response("Error: " + e);
+    });
+  }());
+});
+    
+
+
