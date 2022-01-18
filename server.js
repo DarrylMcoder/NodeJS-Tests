08624870ecbf44f70bc8f4833692b0345c98bc6f
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-01-17 21:09:48 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require("express"),
     unblocker = require("unblocker"),
     encodeContent = require('./encodeContent.js'),
+    caesarShift = require('./caesarShift.js').caesarShift,
     origin = require('./origin.js'),
     accessControlAllowOrigin = require('./access-control-allow-origin.js'),
     serveStatic = require('serve-static'),
     port = process.env.PORT || 80,
     app = express();
+
+app.use(function(req, res, next) {
+  req.url = decodeUrl(req.url);
+  next();
+});
 
 app.use(
   unblocker({
@@ -35,3 +41,13 @@ app.use("/",serveStatic("public", {
 app.listen(port);
 
 console.log("app listening on port "+ port);
+
+function decodeUrl(url) {
+  let separator = 'proxy/';
+  if(!url.includes(separator)) {
+    return;
+  }
+  let proxyUrl = url.slice(url.indexOf(separator) + separator.length);
+  let enc = caesarShift(proxyUrl, -1);
+  return url.replace(proxyUrl, enc);
+}
