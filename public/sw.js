f183cb7930207106f0636ba9132b6ec3e429f61b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-01-21 09:08:22 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(async function() {
     try {
     var url = encodeUrl(event.request.url);
-      return url;
+      return "Event.req.Url:" + event.request.url + "Url" + url;
     var req = event.request;
     var init =     {
       method: req.method,
